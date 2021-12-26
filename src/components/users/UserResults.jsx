d00c45626e78cadbccdcc2391e7f6dbb306df08b
PaulNorthern <sculltakers111@gmail.com>
2021-12-26 14:38:31 +0300

    UserComponent

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+
+// when the components loads
+function UserResults() {
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    const fetchUsers = async () => {
+        const url = process.env.REACT_APP_GITHUB_URL + '/users';
+        const token = `token ${process.env.REACT_APP_GITHUB_TOKEN}`;
+       
+        console.log(url)
+        console.log(token)
+
+        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+                headers: {
+                    Authorization: token,
+                }
+            })   
+            
+            const data = await response.json()
+            setUsers(data);
+            setLoading(false);
+    }
+
+    if (!loading) {
+        return (
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 
+                lg:grid-cols-3 md:grid-cols-2'>
+                    {users.map((user) => (
+                        <h3>{user.login}</h3>
+                    ))}
+                </div>
+        )
+    } else {
+        return <h3>Loading...</h3>
+    }
+    
+}
+
+export default UserResults
